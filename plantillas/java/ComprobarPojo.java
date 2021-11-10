557ba286bf59be87dd81171e493648ee612da46b
= <=>
2021-11-10 21:47:55 +0100

    Template added

---
+++ plantillas/java/ComprobarPojo.java
@@ -0,0 +1,16 @@
+public class ComprobarPojo {
+    private static Scanner sc;
+
+    public static void main(String[] args) {
+        sc = new Scanner(System.in);
+
+
+    }
+
+    private static void crear(int index) {
+        System.out.println("Introduce los datos del " + index + " :");
+        System.out.print(": ");
+        sc.nextLine();
+
+        // return new ();
+    }

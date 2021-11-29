e47f912835b47c189612d5f1bdff7be0145e2bf5
jkutkut <jorgeregonzalez@gmail.com>
2021-11-29 10:57:51 +0100

    esPrimo now works with all int values

--- plantillas/java/Primo.java
+++ plantillas/java/Primo.java
@@ -2,6 +2,10 @@ public class Primo {
     private static boolean esPrimo(int n) {
 		double end = Math.sqrt(n);
 		
+		if (n <= 1) {
+			return false;
+		}
+								}
 		for (int c = 2; c <= end; c++) { // Sólo es necesario hacer hasta la raiz del número
 			if (n % c == 0) { // Si es divisible
 				return false;

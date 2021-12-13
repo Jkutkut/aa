c9c50fd57a15ebe739707917ec69a8e020315fe2
jkutkut <jorgeregonzalez@gmail.com>
2021-12-13 21:53:04 +0100

    primo template fixed

--- plantillas/java/Primo.java
+++ plantillas/java/Primo.java
@@ -1,11 +1,11 @@
 public class Primo {
-    private static boolean esPrimo(int n) {
-		double end = Math.sqrt(n);
+	private static boolean esPrimo(int n) {
+		double end;
 		
 		if (n <= 1) {
 			return false;
 		}
-								}
+		end = Math.sqrt(n);
 		for (int c = 2; c <= end; c++) { // Sólo es necesario hacer hasta la raiz del número
 			if (n % c == 0) { // Si es divisible
 				return false;

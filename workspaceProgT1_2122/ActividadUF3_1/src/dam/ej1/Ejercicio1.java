7c16d2665775a2e613195f8c699bd79bdf27b1ba
jkutkut <jorgeregonzalez@gmail.com>
2021-12-14 07:58:17 +0100

    ./ActividadUF3_1/src/dam/ej1/Ejercicio1.java added to repository

---
+++ workspaceProgT1_2122/ActividadUF3_1/src/dam/ej1/Ejercicio1.java
@@ -0,0 +1,29 @@
+package dam.ej1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// primera opción
+		int a = 'a';
+		System.out.println(a);
+
+		// segunda opción
+		// int pi = 3.14; NO FUNCIONA
+		
+		// tercera opción
+		// double pi = 3,14; NO FUNCIONA
+		
+		// cuarta opción
+		boolean esCierto = (1 == 4);
+		System.out.println(esCierto);
+
+		// quinta opción
+		// boolean esCierto = (97 == 'a' == 97); NO FUNCIONA
+		
+		// sexta opción
+		boolean esCierto2 = (97 == 'a' == true);
+		System.out.println(esCierto2);
+
+	}
+
+}

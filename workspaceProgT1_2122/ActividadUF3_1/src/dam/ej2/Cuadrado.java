611dcfb0e5ba447707f60fef50263a36b3939377
jkutkut <jorgeregonzalez@gmail.com>
2021-12-14 07:58:17 +0100

    ./ActividadUF3_1/src/dam/ej2/Cuadrado.java added to repository

---
+++ workspaceProgT1_2122/ActividadUF3_1/src/dam/ej2/Cuadrado.java
@@ -0,0 +1,10 @@
+package dam.ej2;
+
+public class Cuadrado {
+	public static void main(String [] args) {
+		int numero = 2; 
+		int cuad = numero * numero;
+		System.out.println("EL CUADRADO DE " + numero + " ES: " + cuad);
+	}
+}
+

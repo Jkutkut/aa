83190293553eb2fde74166c89d7d087e3c95c9c2
jkutkut <jorgeregonzalez@gmail.com>
2021-12-14 07:58:17 +0100

    ./PruebasUF3_1/src/dam/pruebas/operadores/PruebasOpUnit.java added to repository

---
+++ workspaceProgT1_2122/PruebasUF3_1/src/dam/pruebas/operadores/PruebasOpUnit.java
@@ -0,0 +1,19 @@
+package dam.pruebas.operadores;
+
+public class PruebasOpUnit {
+
+	public static void main(String[] args) {
+		int m = 2;
+		int n = 5;
+		System.out.println("m = " + m + " y n = " + n);
+		
+		m++; // m=3
+		System.out.println("m++ --> " + m);
+		
+		n--; // n=4
+		System.out.println("n-- --> " + n);
+
+
+	}
+
+}

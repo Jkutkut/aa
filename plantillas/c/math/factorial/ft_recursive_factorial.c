ed00a30c06608340f03945fddac46838f5d27bf5
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-18 10:03:45 +0100

    factorial functions added

---
+++ plantillas/c/math/factorial/ft_recursive_factorial.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 10:03:09 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/01/18 10:03:16 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb <= 1)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}

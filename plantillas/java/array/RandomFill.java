030dbdde04d2df981f080d1399dad78861e2ef33
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-26 21:50:32 +0100

    RandomFill.java added

---
+++ plantillas/java/array/RandomFill.java
@@ -0,0 +1,13 @@
+package array;
+
+public class RandomFill {
+	private static void randomFill(int[][] matriz, int maxVal) {
+		Random r = new Random();
+		
+		for (int i = 0, j; i < matriz.length; i++) {
+			for (j = 0; j < matriz[i].length; j++) {
+				matriz[i][j] = r.nextInt(maxVal);
+			}
+		}
+	}
+}

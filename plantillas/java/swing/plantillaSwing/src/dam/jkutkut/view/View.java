5cfee82c3ec1810cb35fd0bb108cb688051247a6
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-30 11:02:00 +0200

    view updated

--- plantillas/java/swing/plantillaSwing/src/dam/jkutkut/view/View.java
+++ plantillas/java/swing/plantillaSwing/src/dam/jkutkut/view/View.java
@@ -5,12 +5,24 @@ import javax.swing.JFrame;
 import dam.jkutkut.control.Controller;
 
 public class View extends JFrame {
+	
+	private static final String TITLE = "";
+	private static final int WIDTH = 500;
+	private static final int HEIGHT = 500;
+	
 	public View() {
 		init();
 	}
 	
 	private void init() {
+		setTitle(TITLE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
 		// TODO
+		
+		setSize(WIDTH, HEIGHT);
+		setLocationRelativeTo(null);
 	}
 	
 	public void showWindow() {
